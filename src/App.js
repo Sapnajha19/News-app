30478035d2eb1ed7d3fe6f49a5f9e5e44115fcba
Sapna Jha <sapnajha1919@gmail.com>
2023-04-04 12:43:24 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//commenting out functional based app component bcz we are learning class based component now
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+//rcc-->shortcut code snippet
+import React, { Component } from 'react'
+import Navbar from './Components/Navbar';
+import News from './Components/News';
+
+export default class App extends Component {
+  // c="Sapna";
+  render() {
+    return (
+      <div>
+        {/* Hello my first class based component {this.c} */}
+        <Navbar/>
+        <News pageSize={5} country="in" category="sports"/>
+      </div>
+    )
+  }
 }
 
-export default App;
---
+++ src/Components/News.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+import Spinner from "./Spinner"
+import PropTypes from 'prop-types'
+
+export class News extends Component {
+  static defaultProps = {
+    country: 'in',
+    pageSize:8,
+    category: 'general'
+  }
+
+  static propTypes = {
+    county: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string
+  }
+
+  constructor() {
+    super();
+    console.log("Hello I am a constructor from News component");
+    this.state = {
+      // articles:this.articles,
+      articles: [],
+      loading: false,
+      page: 1
+    }
+  }
+
+  async componentDidMount() {  //anything inside componentDidMount renders after the elements inside render()
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2330cca42bb242568012a807cd8e5e2b&pageSize=${this.props.pageSize}`;
+    this.setState({
+      loading: true
+    })
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults, loading: false });
+  }
+
+  handlePreviousClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2330cca42bb242568012a807cd8e5e2b&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      loading: true
+    })
+
+
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+      loading: false
+    })
+
+  }
+
+  handleNextClick = async () => {
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+
+    }
+    else {
+      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2330cca42bb242568012a807cd8e5e2b&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+      this.setState({
+        loading: true
+      })
+      let data = await fetch(url);
+      let parsedData = await data.json();
+
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+        loading: false
+      })
+
+    }
+  }
+
+  render() {
+    return (
+      <div className="container my-3">
+        <h2 className="text-center">NewsMonkey- Top headlines</h2>
+        {this.state.loading && <Spinner />}
+        <div className="row">
+          {!this.state.loading && this.state.articles.map((element) => {
+            return <div className="col-md-4" key={element.url}>
+              <NewsItem title={element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 88) : ""} imageUrl={element.urlToImage} newsUrl={element.url} />
+            </div>
+          })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePreviousClick}>&larr; Previous</button>
+          <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / 20)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default News
